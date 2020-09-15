9fbf0478c25e89c1efe4d9958d68955773183ef5
RaihanDewonEman <raihan11011997@gmail.com>
2020-09-15 20:41:45 +0600

    Almost complete

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,11 @@ from django.db import models
 # Create your models here.
 
 class Blog(models.Model):
-    title = models.CharField(max_length=30)
+    title = models.CharField(max_length=40)
     image = models.ImageField(upload_to='images/')
     post = models.TextField()
     postdate = models.DateField()
+
+
+    def __str__(self):
+        return self.title
